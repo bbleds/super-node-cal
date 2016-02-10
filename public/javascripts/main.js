9f95766e4757669121575e8d355cecef6400ccdf
Ben Bledsoe <bledsoedesigns@gmail.com>
2016-02-10 17:14:13 -0600

    added handlers for errors

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -3,20 +3,24 @@
 $("html").on("click", "#getGeneration", function(){
 
 	//if year and month is entered
-	if($("#yearInput").val() !== "" && $("#monthInput").val() !== ""){
+		//parseInt and check range for both as validation (will not pass with wrong range or letters included, etc)
+	if(parseInt($("#yearInput").val()) >= 1753 && parseInt($("#yearInput").val()) <= 9999 && parseInt($("#monthInput").val()) >= 1 && parseInt($("#monthInput").val()) <= 12)
+	{
 		//get month of specfied year from api
 		$.get("/api/"+$("#yearInput").val()+"/"+$("#monthInput").val(), function(data){
 			$("#generationOutput").html(data)
 		})
 
-	} else if($("#yearInput").val() !== "") {
-
-		//get year from api
+	}
+	else if(parseInt($("#yearInput").val()) >= 1753 && parseInt($("#yearInput").val()) <= 9999)
+	{
+		//get month of specfied year from api
 		$.get("/api/"+$("#yearInput").val(), function(data){
 			$("#generationOutput").html(data)
 		})
-
-	} else {
+	}
+	else
+	{
 		$("#generationOutput").html("There was an error with that request, please try again")
 	}
 })
