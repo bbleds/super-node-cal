dd0ae1c25664f4cb063a0ade8360a7db3e717cdd
Ben Bledsoe <bledsoedesigns@gmail.com>
2016-02-10 14:08:53 -0600

    adding sass middleware compiler

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
 const express = require("express");
 const path = require("path")
+const sassMiddleware = require('node-sass-middleware');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 //configure views engine for jade
 app.set("view engine", "jade")
+
+//sass middleware -> sass will be compiled when linked in views/header.jade
+app.use(sassMiddleware({
+    /* Options */
+    src: path.join(__dirname, 'public'),
+    dest: path.join(__dirname, 'public'),
+    //this is whitespace indent rather than curly braces
+    indentedSyntax: true,
+    debug: true,
+    outputStyle: 'compressed'
+}));
 
 //look for files in /public directory
 app.use(express.static(path.join(__dirname, '/public')));
